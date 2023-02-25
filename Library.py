0a8949bb5f172c741722206e9e99a4031169498a
maherasim <asimriazasim107@gmail.com>
2023-02-25 20:28:23 +0500

    add  remove function to remove book from library

--- Library.py
+++ Library.py
@@ -1,9 +1,29 @@
 library = {}
 
-def add_book(isbn, title, author, publisher, publication_date):
+def add_book(isbn, title, author, publisher,  pub_date):
     """Adds a book to the library"""
-    library[isbn] = {"title": title, "author": author, "publisher": publisher, "publication_date": publication_date}
-    print("Book added successfully.")
+    isbn = input("Enter the book's ISBN: ")
+    if isbn in library:
+        print("The book already exists in the library!")
+    else:
+        title = input("Enter the book's title: ")
+        author = input("Enter the book's author: ")
+        publisher = input("Enter the book's publisher: ")
+        pub_date = input("Enter the book's publication date (YYYY-MM-DD): ")
+        
+        library[isbn] = {"title": title, "author": author, "publisher": publisher, 
+                         "publication date": pub_date}
+        print("The book has been added to the library.")
+
+def remove_book():
+    """Remove a book from the library"""
+    isbn = input("Enter the book's ISBN to remove: ")
+    if isbn in library:
+        del library[isbn]
+        print("The book has been removed from the library.")
+    else:
+        print("The book was not found in the library.")
+
 
 def show_books():
     """Shows the list of books in the library"""
@@ -19,7 +39,9 @@ while True:
     print("\nWhat would you like to do?")
     print("1. Show books in the library")
     print("2. Add a new book to the library")
-    print("3. Quit")
+    print("3. Remove Books from library")
+
+    print("4. Quit")
     choice = input("> ")
     
     if choice == "1":
