496027828546637199c7968f1b352bc6e99302d6
maherasim <asimriazasim107@gmail.com>
2023-02-26 21:34:33 +0500

    improved functionallity of display function

--- Library.py
+++ Library.py
@@ -1,7 +1,7 @@
 library = {}
 
-def add_book(isbn, title, author, publisher,  pub_date):
-    """Adds a book to the library"""
+def add_book():
+    """Add a book to the library"""
     isbn = input("Enter the book's ISBN: ")
     if isbn in library:
         print("The book already exists in the library!")
@@ -10,7 +10,7 @@ def add_book(isbn, title, author, publisher,  pub_date):
         author = input("Enter the book's author: ")
         publisher = input("Enter the book's publisher: ")
         pub_date = input("Enter the book's publication date (YYYY-MM-DD): ")
-        
+       
         library[isbn] = {"title": title, "author": author, "publisher": publisher, 
                          "publication date": pub_date}
         print("The book has been added to the library.")
@@ -33,43 +33,41 @@ def search_book():
             print(f"{key.capitalize()}: {value}")
     else:
         print("The book was not found in the library.")
-        
 
-def show_books():
-    """Shows the list of books in the library"""
-    if len(library) == 0:
-        print("The library is empty.")
-    else:
-        print("Books in the library:")
+def display_books():
+    """Display all books in the library"""
+    if library:
+        print("Library books:")
         for isbn, details in library.items():
-            print(f"{details['title']} by {details['author']} (ISBN: {isbn})")
-
-# Main program loop
-while True:
-    print("\nWhat would you like to do?")
-    print("1. Show books in the library")
-    print("2. Add a new book to the library")
-    print("3. Remove Books from library")
-    print("4. Search Books from library")
-    print("5. Quit")
-    choice = input("> ")
-    
-    if choice == "1":
-        show_books()
-    elif choice == "2":
-        print("\nPlease enter the details of the book:")
-        isbn = input("ISBN: ")
-        title = input("Title: ")
-        author = input("Author: ")
-        publisher = input("Publisher: ")
-        publication_date = input("Publication Date: ")
-       
-        add_book(isbn, title, author, publisher, publication_date)
-    elif choice == "3":
-        remove_book()
-    elif choice == "4":
-        search_book()
-    elif choice == "5":
-        break
+            print(f"ISBN: {isbn}")
+            for key, value in details.items():
+                print(f"{key.capitalize()}: {value}")
+            print("")
     else:
-        print("Invalid choice. Please try again.")
+        print("The library is empty.")
+
+def main():
+    """Main program loop"""
+    while True:
+        print("\nLibrary Management System")
+        print("1. Add book")
+        print("2. Remove book")
+        print("3. Search book")
+        print("4. Display all books")
+        print("5. Quit")
+        choice = input("Enter your choice (1-5): ")
+        if choice == "1":
+            add_book()
+        elif choice == "2":
+            remove_book()
+        elif choice == "3":
+            search_book()
+        elif choice == "4":
+            display_books()
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice!")
+
+if __name__ == "__main__":
+    main()
